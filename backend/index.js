49419f9218485b5bde3e9bd8a11b50e31b375498
ShiyaShivani <siyashvn@gmail.com>
2024-09-14 13:39:55 +0530

    added backend deployed url

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://the-cinema-house-pwmr.vercel.app"],
     methods:["POST","GET","PATCH","PUT","DELETE"],
     credentials:true
   }));
